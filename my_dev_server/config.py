8fe82e26171dac1529390e53d481e8cea2716c6f
Evgeny Sikachev <esikachev@mirantis.com>
2016-12-15 19:50:45 +0400

    Added logger
    Added logger for my-dev-server project.
    Close #9

--- my_dev_server/config.py
+++ my_dev_server/config.py
@@ -1,25 +1,26 @@
 import sys
 
 from oslo_config import cfg
-from oslo_log import log
 
 CONF = cfg.CONF
 
 
-db_opts = [
+opts = [
     cfg.StrOpt('db_url',
                default='mysql+pymysql://root@localhost/my_dev',
-               help='Url of DB for connection.')
+               help='Url of DB for connection.'),
+    cfg.StrOpt('log_dir',
+               default='/var/log/my-dev-server',
+               help='Directory name for logs.')
 ]
 
-CONF.register_opts(db_opts)
-
-log.register_options(CONF)
+CONF.register_opts(opts)
 
 
 def list_opts():
-    return {'DEFAULT': db_opts}
+    return {'DEFAULT': opts}
 
 
 def parse_config():
     CONF(project='my-dev-server', default_config_files=sys.argv[2:])
+
---
+++ my_dev_server/logger.py
@@ -0,0 +1,56 @@
+import functools
+import logging
+import os
+import traceback
+
+from oslo_config import cfg
+from my_dev_server import config
+
+config.parse_config()
+CONF = cfg.CONF
+
+
+if not os.path.exists(CONF.log_dir):
+    os.makedirs(CONF.log_dir)
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s %(filename)s:'
+                    '%(lineno)d -- %(message)s',
+                    filename=os.path.join(CONF.log_dir, 'my-dev-server.log'),
+                    filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s %(filename)s:'
+                              '%(lineno)d -- %(message)s')
+console.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(console)
+
+
+def debug(logger):
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger.debug(
+                "Calling: {} with args: {} {}".format(
+                    func.__name__, args, kwargs
+                )
+            )
+            try:
+                result = func(*args, **kwargs)
+                logger.debug(
+                    "Done: {} with result: {}".format(func.__name__, result))
+            except BaseException as e:
+                logger.error(
+                    '{func} raised: {exc!r}\n'
+                    'Traceback: {tb!s}'.format(
+                        func=func.__name__, exc=e, tb=traceback.format_exc()))
+                raise
+            return result
+        return wrapped
+    return wrapper
+
+
+logwrap = debug(logger)
