de8150456a431bbbb9947cc2a90ff0173afc91f1
Evgney Sikachev <esikachev@mirantis.com>
2017-02-08 23:23:33 +0400

    Update common.py

--- my_dev_server/server/common.py
+++ my_dev_server/server/common.py
@@ -14,15 +14,15 @@ def check_user_correct(username=None, email=None, password=None):
 
     if username and (len(username) < CONF.MINIMAL_USERNAME_LEN):
         err_message = "Username len required > %s" % CONF.MINIMAL_USERNAME_LEN
-        LOG.debug(err_mess)
+        LOG.debug(err_message)
         raise exceptions.LengthRequired(err_message)
 
     if email and (len(email) < CONF.MINIMAL_EMAIL_LEN):
         err_message = "Email len required > %s" % CONF.MINIMAL_EMAIL_LEN
-        LOG.debug(err_mess)
+        LOG.debug(err_message)
         raise exceptions.LengthRequired(err_message)
 
     if password and (len(password) < CONF.MINIMAL_PASSWORD_LEN):
         err_message = "Password len required > %s" % CONF.MINIMAL_PASSWORD_LEN
-        LOG.debug(err_mess)
+        LOG.debug(err_message)
         raise exceptions.LengthRequired(err_message)
