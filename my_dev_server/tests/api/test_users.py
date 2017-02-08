d13417fb85a467e6d74163f3ca53cc51b3645a21
Evgney Sikachev <esikachev@mirantis.com>
2017-02-08 21:19:17 +0400

    Refactoring of api-tests (#57)
    We have an errors in structure of api-tests.
    Close #56

--- my_dev_server/tests/api/test_users.py
+++ my_dev_server/tests/api/test_users.py
@@ -9,25 +9,10 @@ class TestUsers(base.Base):
     def test_create_delete_user(self):
         """Scenario:
           - create user
-          - check that user created successfully
           - delete user
-          - check that user deleted
         """
-        url = self.url + '/users'
-        data = {
-            "username": utils.rand_name('user'),
-            "email": utils.rand_name('email'),
-            "password": utils.rand_name('pass')
-        }
-        user = requests.post(url, json=data)
-        self.assertEqual(200, user.status_code)
-        user = user.json()
-        self.assertEqual(data['username'], user['username'])
-        self.assertEqual(data['email'], user['email'])
-        self.assertEqual(data['password'], user['password'])
-        url = self.url + '/users/%s' % user['id']
-        request = requests.delete(url)
-        self.assertEqual(200, request.status_code)
+        data, user = self.create_user()
+        self.delete_user(user['id'])
 
     def test_get_user(self):
         """Scenario:
@@ -35,29 +20,19 @@ class TestUsers(base.Base):
           - get user
           - delete user
         """
-        user = self._create_user(
-            utils.rand_name('user'),
-            utils.rand_name('email'),
-            utils.rand_name('pass'))
-        url = self.url + '/users/%s' % user['id']
-        get_user = requests.get(url)
+        data, user = self.create_user()
+        get_user = self.get_user(user['id'])
+        
+        self.check_response(user, get_user)
 
-        self.assertEqual(200, get_user.status_code)
-        self.assertEqual(user, get_user.json())
-        self._delete_user(user)
+        self.delete_user(user['id'])
 
     def test_get_non_existing_user(self):
         """Scenario:
           - Try to get non existing user
           - check that status code correct (404)
         """
-        fake_user = {
-            "id": utils.rand_name('123'),
-            "username": utils.rand_name('username'),
-            "email": utils.rand_name('email'),
-            "password": utils.rand_name('pass')
-        }
-        self._get_user(fake_user, expected_code=404)
+        self.get_user('11111', expected_code=404)
 
     def test_create_user_exist_username(self):
         """Scenario:
@@ -66,16 +41,9 @@ class TestUsers(base.Base):
           - Check that exit code correct (409)
           - delete user
         """
-        user = self._create_user(
-            'username',
-            utils.rand_name('email'),
-            utils.rand_name('pass'))
-        self._create_user(
-            'username',
-            utils.rand_name('email'),
-            utils.rand_name('pass'),
-            expected_code=409)
-        self._delete_user(user)
+        data, user = self.create_user(username='username')
+        self.create_user(username='username', expected_code=409)
+        self.delete_user(user['id'])
 
     def test_create_user_without_username(self):
         """Scenario:
@@ -83,15 +51,9 @@ class TestUsers(base.Base):
           - Check that exit code correct (400)
           - Check that error message is correct
         """
-        url = self.url + '/users'
-        data = {
-            "email": utils.rand_name('email'),
-            "password": utils.rand_name('pass')
-        }
-        message = 'Please provide: username, password and email'
-        user = requests.post(url, json=data).json()
-        self.assertEqual(400, user['status_code'])
-        self.assertEqual(message, user['message'])
+        self.create_user(remove_field='username', expected_code=400,
+                         error_msg='Please provide: username, password and '
+                                   'email')
 
     def test_create_user_not_correct_creds(self):
         """Scenario:
@@ -102,36 +64,17 @@ class TestUsers(base.Base):
           - create user with not correct email
           - check that exit code and message are correct
         """
-        url = self.url + '/users'
-        data = {
-            "username": utils.rand_name(length=2),
-            "email": utils.rand_name('email'),
-            "password": utils.rand_name('pass')
-        }
-        message = 'Username len required > 2'
-        user = requests.post(url, json=data).json()
-        self.assertEqual(411, user['status_code'])
-        self.assertEqual(message, user['message'])
+        self.create_user(username=utils.rand_name(length=2),
+                         error_msg='Username len required > 2',
+                         expected_code=411)
 
-        data = {
-            "username": utils.rand_name('username'),
-            "email": utils.rand_name('email'),
-            "password": utils.rand_name(length=4)
-        }
-        message = 'Password len required > 5'
-        user = requests.post(url, json=data).json()
-        self.assertEqual(411, user['status_code'])
-        self.assertEqual(message, user['message'])
+        self.create_user(password=utils.rand_name(length=4),
+                         error_msg='Password len required > 5',
+                         expected_code=411)
 
-        data = {
-            "username": utils.rand_name('username'),
-            "email": utils.rand_name(length=2),
-            "password": utils.rand_name('password')
-        }
-        message = 'Email len required > 4'
-        user = requests.post(url, json=data).json()
-        self.assertEqual(411, user['status_code'])
-        self.assertEqual(message, user['message'])
+        self.create_user(email=utils.rand_name(length=2),
+                         error_msg='Email len required > 4',
+                         expected_code=411)
 
     def test_create_user_exist_email(self):
         """Scenario:
@@ -140,13 +83,11 @@ class TestUsers(base.Base):
           - Check that exit code correct (409)
           - delete user
         """
-        user = self._create_user(
-            utils.rand_name('username'),
-            'email',
-            utils.rand_name('pass'))
-        self._create_user(
-            utils.rand_name('username'),
-            'email',
-            utils.rand_name('pass'),
-            expected_code=409)
-        self._delete_user(user)
+        data, user = self.create_user(email='email')
+        self.create_user(email='email', expected_code=409)
+        self.delete_user(user['id'])
+
+    def check_response(self, data, user):
+        self.assertEqual(data['username'], user['username'])
+        self.assertEqual(data['email'], user['email'])
+        self.assertEqual(data['password'], user['password'])
